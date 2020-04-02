370de1bae4a76cfb9145682a9b69d14acc4af3da
Roel van den Bussche <roel@moreteq.com>
2020-04-02 10:26:16 +0200

    Create SENDTEQ - Exchange setup.ps1

---
+++ SENDTEQ - Exchange setup.ps1
@@ -0,0 +1,12 @@
+﻿# This script adds the following to your Exchange environment:
+# - Outbound connector
+# - Inbound Connector
+# - Transport rule disabled by default
+#
+# After running the script, you will have to manually check and activate the transport rule.
+#
+# Web: https://sendteq.com
+# Contact: support@sendteq.com
+# Use at own risk!
+
+# Not yet here...
